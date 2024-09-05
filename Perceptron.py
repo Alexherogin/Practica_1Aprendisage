5b7d02802efa2311093a36dfc1f8ae30a3a34e01
Alexherogin <160820160+Alexherogin@users.noreply.github.com>
2024-09-05 03:40:42 +0000

    Percetron base V1.2

--- Perceptron.py
+++ Perceptron.py
@@ -4,32 +4,33 @@
 import random
 
 class Perceptron:
-    def __int__(self, num_inputs):
+     def __init__(self, num_inputs):
         self.weights = [random.uniform(-1,1) for _ in range (num_inputs)]
         self.bias = random.uniform(-1,1)
 
-    def predict(self,inputs ):
-            activation= self.bias 
+     def predict(self,inputs ):
+            activation = self.bias 
             for i in  range (len(inputs)):
                 activation += inputs[i] + self.weights[i]
             return 1 if activation >= 0 else 0 
-    def train(self,inputs,target ):
+    
+     def train(self,inputs,target ):
         output = self.predict(inputs)
         error = target - output 
         self.bias +=  error 
         for i in range(len(self.weights)):
-             self.weights[i] += error * inputs[i] 
-    def get_weights(self):
+             self.weights[i] += error * inputs[i]
+
+     def get_weights(self):
          return self.weights
     
-    def seve_weights(self,filename):
+     def seve_weights(self,filename):
          with open(filename,"w") as f:
           f.write(f"{self.bias}\n")
           for w in self.weights:
                f.write(f"{w}\n")
 
-    
-    def load_weights(self,filename):
+     def load_weights(self,filename):
          with open(filename,"r") as f:
               self.bias= float(f.readline())
               self.weights= [float(line)for line in f.readline() ]
@@ -42,7 +43,7 @@ class Perceptron:
 
 # Ejemplo de uso 
 
-perceptron = Perceptron
+perceptron = Perceptron(2)
 
 #si exixte el archivo, cargha pesos
 try:
